2d27530d4ef92bab07e075ce17bac3ae7c5960e1
Ivan <ivanzhao8@gmail.com>
2020-08-02 04:30:12 -0400

    New changes!

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,95 +1,174 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Input } from "antd";
 import TwitterLogo from "../../assets/twitter.png";
+import illustration from "../../assets/undraw.svg";
 
-import { Collapse, Spin } from "antd";
+import { Collapse, Spin, Button, Tag } from "antd";
 
 const { Panel } = Collapse;
 
-const colorMap = {
-  Joy: "",
-  Anger: "",
-  Fear: "",
-  Sadness: "",
-  Analytical: "",
-  Confident: "",
-  Tentative: "",
-};
-
 export default function Index() {
   const [searchValue, setSearchValue] = useState("");
+  const [numberTweets, setNumberTweets] = useState(20);
   const [tweets, setTweets] = useState([]);
   const [sentiment, setSentiments] = useState([]);
-  const [links, setLinks] = useState([]);
   const [users, setUsers] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const [trends, setTrends] = useState([]);
+  const [lat, setLat] = useState(37.7749);
+  const [long, setLong] = useState(122.4194);
+
+  const setPositions = (position) => {
+    setLat(position.coords.latitude);
+    setLong(position.coords.longitude);
+  };
+  const getTrends = () => {
+    if (window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(setPositions);
+      fetch(`/trends/${lat}/${long}`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) =>
+          setTrends(data.trends[0].trends.slice(Math.random() * numberTweets))
+        );
+    } else {
+      fetch(`/trends/${lat}/${long}`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => setTrends(data.trends[0].trends.slice(0, 3)));
+    }
+  };
 
   const setInputValue = (e) => {
     setSearchValue(e.target.value);
   };
 
+  const setNumberValue = (e) => {
+    setNumberTweets(e.target.value);
+  };
+
   const submitQuery = () => {
-    console.log(isLoading);
-    setLoading(isLoading);
-    const searchQuery = `/${searchValue}`;
+    console.log(searchValue);
+    const searchQuery = `/${searchValue}/${numberTweets}`;
+    setLoading(!isLoading);
     fetch(searchQuery)
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         setUsers(data.users);
-        setLinks(data.links);
         setSentiments(data.sentiments);
         setTweets(data.tweets);
+        setLoading(false);
       })
-      .catch((err) => console.log(err));
-    setLoading(isLoading);
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   };
 
   return (
     <div>
       <div style={{ display: "flex" }}>
-        <img src={TwitterLogo} alt="TwitterLogo" />
-        <div style={{ margin: "auto", width: "512px" }}>
-          <div
-            style={{ textAlign: "center", margin: "auto", fontSize: "56px" }}
-          >
-            Feelin' Twitter
-          </div>
-          <div>
-            Feelin' Twitter is a small webapp designed to help you understand
-            and break down the feelings and validity of tweets on important
-            topics. Search for your favorite topics like you would in Twitter
-            and we'll provide the links and the analysis!
-          </div>
+        <img
+          src={TwitterLogo}
+          alt="TwitterLogo"
+          style={{ alignSelf: "center" }}
+        />
+        <div style={{ display: "flex" }}>
+          <div style={{ margin: "0px 128px" }}>
+            <div
+              style={{ textAlign: "center", margin: "auto", fontSize: "56px" }}
+            >
+              Feelin' Twitter
+            </div>
+            <div
+              style={{
+                textAlign: "center",
+                fontSize: "24px",
+                color: "navyblue",
+              }}
+            >
+              Feelin' Twitter is a small webapp designed to help you understand
+              and break down the feelings and validity of tweets on important
+              topics. Search for your favorite topics like you would in Twitter
+              and we'll provide the links and the analysis! (The current number
+              of tweets defaults to 20)
+            </div>
 
-          <Input
-            onChange={setInputValue}
-            onPressEnter={submitQuery}
-            placeholder="Find a hashtag or person to follow and keep track of what they're tweeting"
-            style={{
-              width: "512px",
-              height: "48px",
-              borderRadius: "5px",
-              margin: "24px 0",
-            }}
-          />
+            <img
+              src={illustration}
+              alt="Illustration for twitter graphic"
+              style={{ width: "128px", margin: "10px 0px 10px 475px" }}
+            />
+
+            <div
+              style={{ display: " flex", width: "100%", marginLeft: "128px" }}
+            >
+              <Input
+                onChange={setInputValue}
+                onPressEnter={submitQuery}
+                placeholder="Search a key word or hashtag!"
+                style={{
+                  width: "512px",
+                  height: "48px",
+                  borderRadius: "5px",
+                  margin: "24px 0",
+                }}
+              />
+              <Input
+                onChange={setNumberValue}
+                onPressEnter={submitQuery}
+                default={numberTweets}
+                placeholder="Number of tweets"
+                style={{
+                  width: "256px",
+                  height: "48px",
+                  borderRadius: "5px",
+                  margin: "24px 10px",
+                }}
+              />
+            </div>
+            <Button
+              onClick={getTrends}
+              style={{ display: "flex", margin: "auto" }}
+            >
+              Unsure? Search for some #HOT topics near you
+            </Button>
+          </div>
         </div>
       </div>
 
-      {isLoading && <Spin />}
-
-      <Collapse defaultActiveKey={["1"]}>
-        {tweets &&
-          tweets.map((tweet, i) => {
-            const link = "https://" + links[i];
+      <div style={{ marginLeft: "256px" }}>
+        {trends &&
+          trends.map((trend) => {
             return (
-              <Panel header={"Username:" + users[i]} key={i + 1}>
-                <p>{tweet}</p>
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                  Link to the actual tweet: {links[i]}
-                </a>
-                <div>
+              <Tag style={{ fontSize: "16px", margin: "8px" }}>
+                {trend.name}
+              </Tag>
+            );
+          })}
+      </div>
+
+      {isLoading && (
+        <div style={{ display: "flex", margin: "auto", left: "50%" }}>
+          <Spin
+            size="large"
+            tip="Loading tweets..."
+            style={{ margin: "64px auto", color: "white" }}
+          />{" "}
+        </div>
+      )}
+
+      {tweets &&
+        tweets.map((tweet, i) => {
+          return (
+            <Collapse defaultActiveKey={["1"]} style={{ margin: "4px 24px" }}>
+              <Panel header={"Username: @" + users[i]} key={i + 1}>
+                <p style={{ fontWeight: "500" }}>{tweet}</p>
+                <div style={{ fontSize: "14px" }}>
                   Tones detected:
                   {sentiment[i].document_tone.tones &&
                     sentiment[i].document_tone.tones.map((tone) => {
@@ -101,9 +180,9 @@ export default function Index() {
                     })}
                 </div>
               </Panel>
-            );
-          })}
-      </Collapse>
+            </Collapse>
+          );
+        })}
     </div>
   );
 }
